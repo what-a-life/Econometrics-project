a4b49688261d4edb96ae9f508462ef8621527a41
Jan Piotrowski <mjpiotr54@gmail.com>
2024-05-26 19:29:00 +0200

    feat: cleaned the code

--- initial-modeling.R
+++ initial-modeling.R
@@ -1,27 +1,6 @@
-# previous code
-
-#levels <- c(0, 1, 2, 3, 4, 5, 6)
-#labels <- c("No WiFi", "WiFi Satisfaction 1", "WiFi Satisfaction 2", "WiFi Satisfaction 3", "WiFi Satisfaction 4", "WiFi Satisfaction 5")
-
-#data$`Inflight wifi service` <- as.character(data$`Inflight wifi service`)
-
-data <- data %>% filter(!(data$`Inflight wifi service` %in% 0))
-data$`Inflight wifi service` <- factor(data$`Inflight wifi service`, levels = c(1, 2, 3, 4, 5), ordered = F)
-table(data$`Inflight wifi service`)
-
-data <- data %>% filter(!(data$`Departure/Arrival time convenient` %in% 0))
-data$`Departure/Arrival time convenient` <- factor(data$`Departure/Arrival time convenient`, levels = c(1, 2, 3, 4, 5), ordered = F)
-table(data$`Departure/Arrival time convenient`)
-
-data <- data %>% filter(!(data$`Departure/Arrival time convenient` %in% 0))
-data$`Departure/Arrival time convenient` <- factor(data$`Departure/Arrival time convenient`, levels = c(1, 2, 3, 4, 5), ordered = F)
-table(data$`Departure/Arrival time convenient`)
-
-#######################################################################################
-
+# Libraries
 library(tidyverse)
 library("dplyr")
-
 library("lmtest")
 library("MASS")
 library("mfx")
@@ -31,7 +10,7 @@ library("aod")
 library("logistf")
 library("car")
 library(generalhoslem)
-#library(gofcat)
+
 
 options(scipen = 5)
 
@@ -39,19 +18,23 @@ data <- read_csv('test.csv')
 
 data[!complete.cases(data),]
 
+sum(is.na(data))
+
+# Only 83 missing values so we will just simply remove them
+
 data <- na.omit(data)
 
-#glimpse(data)
+glimpse(data)
 
-#table(data$satisfaction)
+table(data$satisfaction)
+
+# The distribution of dependent variable is pretty even
+
+# Encoding dependent variable to 0 - 1 
 
 data$satisfaction <- ifelse(data$satisfaction == 'satisfied', 1, 0)
 
-#table(data$`Inflight wifi service`)
-
-#hist(log(data$`Flight Distance`))
-
-data <- subset(data, select = c(-...1,-id))
+# Initial variables encoding
 
 data$Gender <- as.factor(data$Gender)
 data$`Customer Type` <- factor(data$`Customer Type`, levels = c("disloyal Customer", "Loyal Customer"), ordered = F)
@@ -77,12 +60,7 @@ variable_names <- c("Inflight wifi service",
                     "Inflight service", 
                     "Cleanliness")
 
-# Plot histograms for each variable
-#par(mfrow=c(4, 4))  # Arrange plots in a 4x4 grid
 
-#for (variable in variable_names) {
-#  hist(data[[variable]], main = variable, xlab = "Satisfaction level")
-#}
 
 
 process_variable <- function(data, variable_name) {
@@ -106,150 +84,34 @@ data <- data %>%
 
 par(mfrow = c(1, 1))
 source("linktest.R")
-
-mylogit <- glm(satisfaction ~ . + I(Age^2) , data=data, family=binomial(link="logit"))
-
-summary(mylogit)
-
-# tests
-null <- glm(satisfaction~1,data = data,family=binomial(link="logit"))
-lrtest(mylogit,null)
-
-
-logitgof(data$satisfaction, fitted(mylogit), g = 10)
-
-o.r.test(mylogit)
-#stukel.test(mylogit)
-
-summary(linktest(mylogit))
-
-mylogit <- glm(satisfaction ~ . + I(Age^2) + `Class`:`Type of Travel` +
-                 
-                 
-                 , data=data, family=binomial(link="logit"))
-
-summary(mylogit)
-data$Cleanliness <- factor(ifelse(data$Cleanliness %in% c(1, 2), 'dissatisfied', 'satisfied'))
-
-szpagat <- glm(satisfaction ~ Gender + `Customer Type` + `Type of Travel` + `Class`
-               + I(log(`Arrival Delay in Minutes`+1))+I(log(`Departure Delay in Minutes`+1))
-               + `Class`:`Type of Travel`
-               + I(Age^2)
-               + `Gate location`
-               + Log_Flight_Distance
-
-               , data=data, family=binomial(link="logit"))
-
-logitgof(data$satisfaction, fitted(szpagat), g = 10)
-
-o.r.test(szpagat)
-stukel.test(szpagat)
-
-summary(linktest(szpagat))
-
-szpagetti2 <- glm(satisfaction ~ 
-                `Customer Type` 
-                + `Arrival Delay in Minutes`
-                + `Departure Delay in Minutes`
-                
-                + `Flight Distance`
-                + `Departure/Arrival time convenient`
-                
-                + `Ease of Online booking`
-                + `Online boarding`
-                
-                + `Seat comfort`
-                + `Leg room service`
-                
-                + Gender
-                
-                + Age:`Gate location`
-                + Age:`Ease of Online booking`
-                + Age:`Checkin service`
-                + Age:`Food and drink`
-                
-                + Cleanliness
-                
-
-
-              ,data = data, family = binomial(link = "logit"))
-
-logitgof(data$satisfaction, fitted(szpagetti2), g = 10)
-
-o.r.test(szpagetti2)
-stukel.test(szpagetti2)
-
-summary(linktest(szpagetti2))
-
-summary(szpagetti2)
-
-
-PseudoR2(szpagetti2)
-
-
-
-
-
-model1 <- glm(satisfaction~
-                `Customer Type`+
-                
-                `Arrival Delay in Minutes`+
-                
-                `Leg room service`+
-                
-                
-                `Flight Distance`+
-                
-                `Departure/Arrival time convenient`+
-                `Ease of Online booking`+
-                
-                
-                `Online boarding`+
-                `Seat comfort`
-              ,
-              data = data, family = binomial(link = "logit"))
-
-
-logitgof(data$satisfaction, fitted(model1), g = 10)
-
-o.r.test(model1)
-stukel.test(model1)
-
-summary(linktest(model1))
-
-summary(model1)
+source("AllGOFTests.R")
 
 
 # ---------------------- model choice ---------------------------------------
 
-# Model choice: logit or ptobit?
+# Model choice: logit or probit?
 
 logit_model <- glm(satisfaction ~ 
                     `Customer Type` 
                   + `Arrival Delay in Minutes`
                   + `Departure Delay in Minutes`
-                  
                   + `Flight Distance`
                   + `Departure/Arrival time convenient`
-                  
                   + `Ease of Online booking`
                   + `Online boarding`
-                  
                   + `Seat comfort`
                   + `Leg room service`
-                  
                   + Gender
-                  
                   + Age:`Gate location`
                   + Age:`Ease of Online booking`
                   + Age:`Checkin service`
                   + Age:`Food and drink`
-                  
                   + Cleanliness
-                  
-                  
-                  
                   ,data = data, family = binomial(link = "logit"))
+
+null <- glm(satisfaction~1,data = data,family=binomial(link="logit"))
+lrtest(logit_model,null)
+
 
 logitgof(data$satisfaction, fitted(logit_model), g = 10)
 
@@ -269,33 +131,21 @@ probit_model <- glm(satisfaction ~
                      `Customer Type` 
                    + `Arrival Delay in Minutes`
                    + `Departure Delay in Minutes`
-                   
                    + `Flight Distance`
                    + `Departure/Arrival time convenient`
-                   
                    + `Ease of Online booking`
                    + `Online boarding`
-                   
                    + `Seat comfort`
                    + `Leg room service`
-                   
                    + Gender
-                   
                    + Age:`Gate location`
                    + Age:`Ease of Online booking`
                    + Age:`Checkin service`
                    + Age:`Food and drink`
-                   
                    + Cleanliness
-                   
-                   
-                   
                    ,data = data, family = binomial(link = "probit"))
 
 logitgof(data$satisfaction, fitted(probit_model), g = 10)
-
-o.r.test(probit_model)
-stukel.test(probit_model)
 
 summary(linktest(probit_model))
 
@@ -360,49 +210,41 @@ linearHypothesis(logit_model, "`Departure Delay in Minutes`= 0")
 
 # P-value > 5%, we fail to reject H0
 
-logit_model1 <- glm(satisfaction ~ 
+final_model <- glm(satisfaction ~ 
                       `Customer Type` 
                     + `Arrival Delay in Minutes`
-                   
-                    
                     + `Flight Distance`
                     + `Departure/Arrival time convenient`
-                    
                     + `Ease of Online booking`
                     + `Online boarding`
-                    
                     + `Seat comfort`
                     + `Leg room service`
-                    
                     + Gender
-                    
                     + Age:`Gate location`
                     + Age:`Ease of Online booking`
                     + Age:`Checkin service`
                     + Age:`Food and drink`
-                    
                     + Cleanliness
-                    
-                    
-                    
                     ,data = data, family = binomial(link = "logit"))
 
-summary(logit_model1)
+summary(final_model)
 
 
 # tests for the new  model
 
-logitgof(data$satisfaction, fitted(logit_model1), g = 10)
+null <- glm(satisfaction~1,data = data,family=binomial(link="logit"))
+lrtest(final_model,null)
 
-o.r.test(logit_model1)
+logitgof(data$satisfaction, fitted(final_model), g = 10)
 
-stukel.test(logit_model1)
+o.r.test(final_model)
 
-summary(linktest(logit_model1))
+stukel.test(final_model)
 
-summary(logit_model1)
+summary(linktest(final_model))
 
-PseudoR2(logit_model1)
+
+PseudoR2(final_model)
 
 # --------------------- marginal effects --------------------------------------
 
@@ -411,24 +253,17 @@ PseudoR2(logit_model1)
 logitmfx(formula = satisfaction ~ 
            `Customer Type` 
          + `Arrival Delay in Minutes`
-         
-         
          + `Flight Distance`
          + `Departure/Arrival time convenient`
-         
          + `Ease of Online booking`
          + `Online boarding`
-         
          + `Seat comfort`
          + `Leg room service`
-         
          + Gender
-         
          + Age:`Gate location`
          + Age:`Ease of Online booking`
          + Age:`Checkin service`
          + Age:`Food and drink`
-         
          + Cleanliness,
          data = data,
          atmean = T)
@@ -440,24 +275,17 @@ logitmfx(formula = satisfaction ~
 logitmfx(formula = satisfaction ~ 
            `Customer Type` 
          + `Arrival Delay in Minutes`
-         
-         
          + `Flight Distance`
          + `Departure/Arrival time convenient`
-         
          + `Ease of Online booking`
          + `Online boarding`
-         
          + `Seat comfort`
          + `Leg room service`
-         
          + Gender
-         
          + Age:`Gate location`
          + Age:`Ease of Online booking`
          + Age:`Checkin service`
          + Age:`Food and drink`
-         
          + Cleanliness,
          data = data,
          atmean = F)
